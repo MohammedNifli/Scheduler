52ac83105ea8852faafb711ea96794baf690c25a
Mohammed Nifli <mohammednifliap@gmail.com>
2025-04-07 16:50:08 +0530

    bug fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,5 @@
-// app/api/auth/[...nextauth]/route.js
-import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
